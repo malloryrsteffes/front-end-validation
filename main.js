f3a146ba11726959baabc72643b04985788289cf
Mal <malrsteffes@gmail.com>
2020-09-23 20:28:25 -0400

    other validations (length, matching) added

--- main.js
+++ main.js
@@ -23,9 +23,13 @@ function showSuccess(input) {
 }
 
 // Validate an email. Thanks, Stackoverflow
-function validateEmail() {
+function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if (re.test(input.value)) {
+    showSuccess;
+  } else {
+    showError(input, "Email is invalid.");
+  }
 }
 
 // Check Required fields
@@ -43,9 +47,35 @@ function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
+// Check Length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters.`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters.`
+    );
+  }
+}
+
+// Check if passwords match
+function matchPasswords(input1, input2) {
+  if (input1.value != input2.value) {
+    showError(input2, `Passwords must match.`);
+  }
+}
+
 // Listen for a form submit
 form.addEventListener("submit", function(e) {
   e.preventDefault();
 
   checkRequired([username, email, password, confirm_password]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 20);
+  checkEmail(email);
+  matchPasswords(password, confirm_password);
 });
